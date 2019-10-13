4f7fd513a6417278c154bc7fb28fadf0240a0256
ssalinascalibar <ssalinascalibar@gmail.com>
2019-10-13 19:29:19 -0300

    modificando routes para device_for :costumers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :users #ruta REST, es lo mismo que las rutas de arriba, pero con menos codigo  
   devise_for :customers
-# es bloque de rutas es reemplazado por resources :users
+
+
+# este bloque de rutas es reemplazado por resources :users
 =begin
 get 'users', to: 'users#index' #muestra todos los usuarios
 get 'users/new' #ruta para crear el nuevo usuario
@@ -9,7 +12,7 @@ post 'users', to: 'users#create'   #ruta para recibir los datos y procesarlos
 get 'users/:id', to: 'users#show' # controlller users con metodo show
 =end
 
-resources :users #ruta REST, es lo mismo que las rutas de arriba, pero con menos codigo
+
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
