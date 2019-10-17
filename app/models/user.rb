4db8227ae2f1a94e188022a06e3066a95705c97e
ssalinascalibar <ssalinascalibar@gmail.com>
2019-10-17 17:28:44 -0300

    mod relaciones model User y f.select en form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   
   has_many :isapres # un usuario busca muchas isapres
 
-  belongs_to :city # usuario pertenece 1 ciudad
+  #belongs_to :city # usurio pertenece 1 ciudad
 
 end
